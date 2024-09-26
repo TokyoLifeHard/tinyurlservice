6f63facced5e428a52051057ee703e1734476f34
Kawaiiskii <antonzhadnov@gmail.com>
2024-09-26 19:14:07 +0300

    init commit

---
+++ src/main/java/ru/petprojects/tinyurlservice/TinyurlserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.petprojects.tinyurlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyurlserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyurlserviceApplication.class, args);
+	}
+
+}
